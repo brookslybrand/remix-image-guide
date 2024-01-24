bbf40ec4c520d3bb3f484d0b5c7d08ec80569969
Brooks Lybrand <brookslybrand@gmail.com>
2024-01-24 11:55:37 -0600

    Start readme and add assets

--- README.md
+++ README.md
@@ -1,36 +1,19 @@
-# Welcome to Remix + Vite!
+# Remix Image Strategies
 
-📖 See the [Remix docs](https://remix.run/docs) and the [Remix Vite docs](https://remix.run/docs/en/main/future/vite) for details on supported features.
+This is a WIP repo. Here are some things I'm going to try
 
-## Development
+## Sharp
 
-Run the Vite dev server:
+[sharp](https://github.com/lovell/sharp) is library for "High performance Node.js image processing". There is already an official [Remix example](https://github.com/remix-run/examples/tree/main/image-resize) using it, though it could be improved.
 
-```shellscript
-npm run dev
-```
+## Unpic
 
-## Deployment
+[Unpic](https://unpic.pics/img/react/) seems like a nice option if you're already using an image CDN (which you probably should be unless you really want to be building your own).
 
-First, build your app for production:
+## Remix-Image
 
-```sh
-npm run build
-```
+[Remix-Image](https://remix-image.mcfarl.in) unfortunately [hasn't been updated in a bit](https://github.com/Josh-McFarlin/remix-image/releases/tag/v1.3.3), so I am really not confident about it as a solution. However, probably good to rule it out
 
-Then run the app in production mode:
+## vite-plugin-image-optimizer
 
-```sh
-npm start
-```
-
-Now you'll need to pick a host to deploy it to.
-
-### DIY
-
-If you're familiar with deploying Node applications, the built-in Remix app server is production-ready.
-
-Make sure to deploy the output of `npm run build`
-
-- `build/server`
-- `build/client`
+[vite-plugin-image-optimizer](https://github.com/FatehAK/vite-plugin-image-optimizer) allows optimizing all images at build time, which for public assets may be what you want
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { isRouteErrorResponse } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +8,61 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+const images = [
+  {
+    src: "images/big_ear.jpg",
+    alt: "Cute dog with one big ear up and one flopped down",
+    width: 3024,
+    height: 4032,
+  },
+  {
+    src: "images/comfy_boy.jpg",
+    alt: "Dog sleeping wrapped in a purple blanket",
+    width: 3024,
+    height: 4032,
+  },
+  {
+    src: "images/good_dogs.jpg",
+    alt: "Two dogs, a large one sitting, and a small one laying down, both looking at the camera",
+    width: 3024,
+    height: 4032,
+  },
+  {
+    src: "images/remix_logo_transparent.png",
+    alt: "Remix",
+    width: 1200,
+    height: 627,
+  },
+  {
+    src: "images/texas_sky.jpg",
+    alt: "Texas sky with clouds and a sunset",
+    width: 5712,
+    height: 4284,
+  },
+];
+
 export default function Index() {
+  isRouteErrorResponse;
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1>Some beautiful images</h1>
+      <div style={{ gap: "1rem", display: "flex", flexWrap: "wrap" }}>
+        {images.map((image) => (
+          <img
+            key={image.src}
+            src={image.src}
+            alt={image.alt}
+            width={image.width}
+            height={image.height}
+            style={{
+              maxWidth: 300,
+              height: "100%",
+              objectFit: "contain",
+              backgroundColor: "peachpuff",
+            }}
+          />
+        ))}
+      </div>
     </div>
   );
 }
